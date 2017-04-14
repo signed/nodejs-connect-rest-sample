8ac7040021989cbe550da54ba7abfe81ae5161e6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2017-04-14 14:27:51 +0200

    move views into src directory

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
